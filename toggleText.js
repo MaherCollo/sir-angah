fc352c820d8614421d4a8f1fa86e6ecbb6e5062b
MaherCollo <89417764+MaherCollo@users.noreply.github.com>
2024-08-03 23:16:00 +0100

    Create toggleText.js
    Ok

---
+++ toggleText.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+/*<![CDATA[*/
+function toggleText(id) {
+  var textElement = document.getElementById(id);
+  if (textElement.classList.contains("hidden")) {
+    textElement.classList.remove("hidden");
+  } else {
+    textElement.classList.add("hidden");
+  }
+}
+/*]]>*/
+</script>
